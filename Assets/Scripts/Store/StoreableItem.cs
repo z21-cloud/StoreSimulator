66f18f84849f4ab2809a4928983bf4f266da7d1f
Sergey Starostin <starostin371@gmail.com>
2026-03-03 12:35:58 +0300

    Refactore: added comments, deleted methods I don't need

--- Assets/Scripts/Store/StoreableItem.cs
+++ Assets/Scripts/Store/StoreableItem.cs
@@ -7,28 +7,45 @@ namespace StoreSimulator.InteractableObjects
 {
     public class StoreableItem : MonoBehaviour, IInteractable, IStoreable, IHoldable
     {
+        [Header("Move settings")]
         [SerializeField] private MoveToPosition mover;
+        [Header("Throw settings")]
         [SerializeField] private ThrowableSettings throwableSettings;
+        [Header("Item's data settings")]
         [SerializeField] private ItemData itemData;
 
+        // properties
+        // Linke Item -> Shelf
         public IShelf CurrentShelf { get; private set; }
+        // To get Item's category
         public ItemCategory Category => itemData != null ? itemData.Category : ItemCategory.None;
         public ItemData Data => itemData;
+        // ThrowForce
         public float ThrowForce => throwableSettings != null ? throwableSettings.GetThrowForce() : 1f;
 
+        // private components
         private Rigidbody rb;
+        private BoxCollider boxCollider;
 
         private void Awake()
         {
+            // cache
             rb = GetComponent<Rigidbody>();
+            boxCollider = GetComponent<BoxCollider>();
         }
 
         public void OnStored(GameObject slot)
         {
             if (slot.TryGetComponent<IShelf>(out var shelf))
             {
-                rb.isKinematic = true;
+                // keep it for now, delete later
+                // don't need this line, cause I enable kinematic only on SetPhysics method
+                //rb.isKinematic = true;
+                
+                // Link shelf and item
                 CurrentShelf = shelf;
+
+                // move to slot position and rotation (coroutine)
                 mover.MoveToSlotPosition(slot.transform);
             }
             else
@@ -45,7 +62,10 @@ namespace StoreSimulator.InteractableObjects
                 return null;
             }
 
+            // stop coroutine to prevert issue, when player goes back, and object follow him
             mover.StopAllCoroutines();
+
+            // reset shelf
             CurrentShelf.Release();
             CurrentShelf = null;
 
@@ -59,6 +79,7 @@ namespace StoreSimulator.InteractableObjects
 
         public void Hold(Transform holdPoint)
         {
+            // set object to player's hand
             SetParentPosition(holdPoint);
 
             // enable gravity & resets physics when hold object
@@ -75,14 +96,23 @@ namespace StoreSimulator.InteractableObjects
             bool isKinematic = false;
             SetPhysics(isKinematic);
 
+            // throw object in camera direction
             if (impulse != Vector3.zero) rb.AddForce(impulse, ForceMode.Impulse);
         }
 
         private void SetPhysics(bool value)
         {
+            // should I reset velocity or not?
             // rb.linearVelocity = Vector3.zero;
             // rb.angularVelocity = Vector3.zero;
+
             rb.isKinematic = value;
+
+            /* 
+             * kinematic = false => object in world, need collider = true
+             * kinematic = true => object in hands, need collider = false
+             */
+            boxCollider.enabled = !value;
         }
 
         private void SetParentPosition(Transform holdPoint)
